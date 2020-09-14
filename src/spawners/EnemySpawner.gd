84f3e8bc6df074150bb23f6e53d83a373ce4486b
Benjo Kho <benjokho@gmail.com>
2020-09-14 20:37:56 +0800

    add alternate frames

--- src/spawners/EnemySpawner.gd
+++ src/spawners/EnemySpawner.gd
@@ -16,26 +16,26 @@ func spawn_enemies():
 	wave_number += 1
 	
 	var num =  1 + floor(wave_number/3)
-	randomize()
+#	randomize()
 	for i in range (num):
-		randomize()
+#		randomize()
 		match (randi() % 5):
-			0,1,2,3:
+			0,1,2,4:
 				var x = 200 + randi() % 450 + 50
 				var y = randi() % 100 - 150 #-150 to -50 is like 0 to 100 - 150
 				var enemy = enemy_resource.instance()
 				add_child(enemy)
-				randomize()
+#				randomize()
 				var s_style = ((randi() % 3 + wave_number/2) % 12) + 1
 				var wait_multipler : float = 1.0- (floor(wave_number/3.0) * 0.1)
 				wait_multipler = max(wait_multipler, 0.05)
 				enemy.setup(Vector2(x,y), Vector2(0, randi() % 75 + 175), s_style, wait_multipler)
-			4: #BIRD
+			3: #BIRD
 				var x = 20 + randi() % 100
 				var y = 80 + randi() % 50#-150 to -50 is like 0 to 100 - 150
 				var enemy = bird_resource.instance()
 				add_child(enemy)
-				randomize()
+#				randomize()
 				var s_style = 0
 				var wait_multipler : float = 1.0 - (floor(wave_number/3.0) * 0.1)
 				wait_multipler = max(wait_multipler, 0.05)
