f9715e4b7f87455c288a32eced58d54531ce726f
Benjo Kho <benjokho@gmail.com>
2020-11-15 06:43:07 +0800

    Use waypoints

--- src/chars/StandardBird.gd
+++ src/chars/StandardBird.gd
@@ -2,15 +2,15 @@ class_name StandardBird
 extends Enemy
 
 export var goal_position : Vector2
-
-
 func setup(pos, goal_pos, s_style):
-	position = pos
-	goal_position = goal_pos
+	self.position = pos
+	
+	waypoints.append(goal_pos)
 	
 	if shoot_style == -1:
 		set_shoot_style(s_style)
 	else:
 		set_shoot_style(shoot_style)
+		
 #	if position.distance_to(goal_position) <= 3 and shot_cooldown_timer.is_stopped():
 #		shot_cooldown_timer.start()
